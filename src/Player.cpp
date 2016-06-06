805b266141fdefdd2f8c2702d47a586786ddb2bb
Grady O'Connell <flipcoder@gmail.com>
2016-06-06 08:56:57 -0700

    new files

---
+++ src/Player.cpp
@@ -0,0 +1,12 @@
+#include "Player.h"
+
+//Player :: Player():
+//    Sprite()
+//{
+    
+//}
+
+//Player :: ~Player()
+//{
+    
+//}
---
+++ src/Player.h
@@ -0,0 +1,26 @@
+#ifndef PLAYER_H_E74SGBRG
+#define PLAYER_H_E74SGBRG
+
+#include "Qor/Sprite.h"
+
+class Player:
+    public Sprite
+{
+    public:
+
+        //Player();
+        //virtual ~Player();
+
+        //Player(const Player&) = default;
+        //Player(Player&&) = default;
+        //Player& operator=(const Player&) = default;
+        //Player& operator=(Player&&) = default;
+
+    private:
+
+        
+
+};
+
+#endif
+
