55eca118cff0b395ad316e92ebe809532a558d10
Grady O'Connell <flipcoder@gmail.com>
2016-06-24 04:37:32 -0700

    moved player code from Game to Player

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include "Thing.h"
+#include "Player.h"
 #include "Qor/BasicPartitioner.h"
 #include "Qor/Input.h"
 #include "Qor/Qor.h"
@@ -23,8 +24,9 @@ Game :: Game(Qor* engine):
     m_pPipeline(engine->pipeline()),
     m_pPartitioner(engine->pipeline()->partitioner()),
     m_pController(engine->session()->active_profile(0)->controller()),
-    m_JumpTimer(engine->timer()->timeline()),
-    m_ShootTimer(engine->timer()->timeline())
+    m_pTimeline(engine->timer()->timeline())
+    //m_JumpTimer(engine->timer()->timeline()),
+    //m_ShootTimer(engine->timer()->timeline())
 {}
 
 
@@ -62,24 +64,23 @@ void Game :: preload() {
         1.0f
     ));
 
-    m_pChar = m_pQor->make<Sprite>("guy.json");
+    m_pChar = make_shared<Player>(
+        m_pQor->resource_path("guy.json"),
+        m_pResources,
+        m_pCamera.get(),
+        m_pController.get(),
+        m_pTimeline,
+        m_pPartitioner,
+        this
+    );
     //m_pChar->texture()->ambient(Color::red(1.0f)*0.5f);
     m_pRoot->add(m_pChar);
-    m_pChar->set_states({"stand","right"});
-    //m_pCamera->position(glm::vec3(-64.0f, -64.0f, 0.0f));
-    m_pChar->position(glm::vec3(0.0f, 0.0f, 1.0f));
-
-    m_pCharFocusRight = make_shared<Node>();
-    m_pCharFocusRight->position(glm::vec3(32.0f, 0.0f, 0.0f));
-    m_pCharFocusLeft = make_shared<Node>();
-    m_pCharFocusLeft->position(glm::vec3(-32.0f, 0.0f, 0.0f));
-    m_pChar->add(m_pCharFocusRight);
-    m_pChar->add(m_pCharFocusLeft);
+    
     m_Players.push_back(m_pChar);
     
     m_pCamera->mode(Tracker::FOLLOW);
     //m_pCamera->threshold(1.0f);
-    m_pCamera->track(m_pCharFocusRight);
+    m_pCamera->track(m_pChar->focus_right());
     m_pCamera->focus_time(Freq::Time::ms(200));
     m_pCamera->focal_offset(vec3(
         -m_pQor->window()->center().x * 1.0f,
@@ -281,14 +282,13 @@ void Game :: preload() {
     for (auto&& player: m_Players) {
         auto _this = this;
 
-        player->event("battery", [_this](const shared_ptr<Meta>&){
-            ++_this->m_Power;
+        player->event("battery", [_this, &player](const shared_ptr<Meta>&){
+            player->battery(1);
         });
         player->event("star", [_this](const shared_ptr<Meta>& m){
             auto typs = m->at<string>("type");
             int typ = 0;
 
-            // TODO: Make this a case statement
             if(typs == "bronze")
                 typ = 0;
             else if(typs == "silver")
@@ -352,8 +352,8 @@ void Game :: preload() {
         CHARACTER, THING, std::bind(&Thing::cb_to_player, _::_1, _::_2)
     );
 
-    m_JumpTimer.set(Freq::Time::ms(0));
-    m_ShootTimer.set(Freq::Time::ms(0));
+    //m_JumpTimer.set(Freq::Time::ms(0));
+    //m_ShootTimer.set(Freq::Time::ms(0));
 }
 
 
@@ -384,7 +384,7 @@ void Game :: setup_thing(std::shared_ptr<Thing> thing) {
 }
 
 
-void Game :: setup_player(std::shared_ptr<Sprite> player) {
+void Game :: setup_player(std::shared_ptr<Player> player) {
     // create masks
     auto n = make_shared<Node>();
     n->name("mask");
@@ -417,20 +417,20 @@ void Game :: setup_player(std::shared_ptr<Sprite> player) {
     player->add(n);
     m_pPartitioner->register_object(n, CHARACTER_SIDES);
 
-    setup_player_to_map(player);
+    //setup_player_to_map(player);
 
     for (auto&& thing: m_Things)
         setup_player_to_thing(player,thing);
 }
 
 
-void Game :: setup_player_to_map(std::shared_ptr<Sprite> player) {
-    if (not m_pMap)
-        return;
-}
+//void Game :: setup_player_to_map(std::shared_ptr<Player> player) {
+//    if (not m_pMap)
+//        return;
+//}
 
 
-void Game :: setup_player_to_thing(std::shared_ptr<Sprite> player, std::shared_ptr<Thing> thing) {}
+void Game :: setup_player_to_thing(std::shared_ptr<Player> player, std::shared_ptr<Thing> thing) {}
 
 
 std::vector<Node*> Game :: get_static_collisions(Node* a) {
@@ -544,7 +544,7 @@ void Game :: enter() {
     m_pInput->relative_mouse(false);
 
     for (auto&& player: m_Players)
-        player->acceleration(glm::vec3(0.0f, 500.0f, 0.0f));
+        player->enter();
 }
 
 
@@ -554,182 +554,9 @@ void Game :: logic(Freq::Time t) {
     if (m_pInput->key(SDLK_ESCAPE))
         m_pQor->quit();
 
-    for (auto&& player: m_Players) {
-        auto feet_colliders = get_static_collisions(
-            player->hook("feetmask").at(0)
-        );
-
-        auto wall_colliders = m_pPartitioner->get_collisions_for(
-            player->hook("sidemask").at(0), STATIC
-        );
-
-        if (not feet_colliders.empty() && wall_colliders.empty()) {
-            auto v = player->velocity();
-            player->velocity(0.0f, v.y, v.z);
-        }
-        
-        auto vel = player->velocity();
-        bool in_air = feet_colliders.empty();
-        bool walljump = feet_colliders.empty() && not wall_colliders.empty();
-
-        if (walljump)
-            player->set_state("walljump");
-        else if (in_air)
-            player->set_state("jump");
-        
-        glm::vec3 move(0.0f);
-
-        if (player->velocity().x > -K_EPSILON && player->velocity().x < K_EPSILON) {
-            if (m_pController->button("left")) {
-                m_pCamera->track(m_pCharFocusLeft);
-                move += glm::vec3(-1.0f, 0.0f, 0.0f);
-            }
-            if (m_pController->button("right")) {
-                m_pCamera->track(m_pCharFocusRight);
-                move += glm::vec3(1.0f, 0.0f, 0.0f);
-            }
-        }
-
-        if (m_pController->button("shoot") && m_ShootTimer.elapsed()) {
-            shoot(player.get());
-        }
-            
-        bool block_jump = false;
-        if (m_pController->button("up") || m_pController->button("jump")) {
-            
-            if (walljump || not in_air || not m_JumpTimer.elapsed()) {
-                float x = 0.0f;
-
-                if (walljump) {
-                    auto last_dir = m_LastWallJumpDir;
-                    auto tile_loc = wall_colliders.at(0)->world_box().center().x;
-
-                    if (tile_loc < player->position(Space::WORLD).x + 4.0f) {
-                        m_LastWallJumpDir = -1;
-                    } else {
-                        m_LastWallJumpDir = 1;
-                    }
-
-                    // prevent "climbing" the wall by checking to make sure last wall jump was a diff direction (or floor)
-                    // 0 means floors, -1 and 1 are directions
-                    if (m_LastWallJumpDir != 0 && last_dir != 0 && m_LastWallJumpDir == last_dir)
-                        block_jump = true;
-                }
-            
-                // jumping from floor or from a different wall
-                if (not block_jump) {
-                    if (not in_air || walljump || not m_JumpTimer.elapsed()) {
-                        player->velocity(glm::vec3(x, -125.0f, 0.0f));
-
-                        if (not in_air || walljump) {
-                            auto sounds = m_pCamera->hook_type<Sound>();
-
-                            if(sounds.empty())
-                                Sound::play(m_pCamera.get(), "jump.wav", m_pResources);
-                            m_JumpTimer.set(Freq::Time::ms(200));
-                        }
-                    }
-                } else {
-                    // jumping on same wall? only allow this if jump timer is still running
-                    if (not m_JumpTimer.elapsed())
-                        player->velocity(glm::vec3(x, -125.0f, 0.0f));
-                }
-            }
-        } else {
-            m_JumpTimer.set(Freq::Time::ms(0));
-        }
-
-        if (not in_air && m_WasInAir)
-            Sound::play(m_pCamera.get(), "touch.wav", m_pResources);
-
-        m_WasInAir = in_air;
-        
-        if (not in_air)
-            m_LastWallJumpDir = 0;
-
-        if (glm::length(move) > K_EPSILON) {
-            if (not in_air)
-                player->set_state("walk");
-
-            move = glm::normalize(move);
-
-            if (move.x < -K_EPSILON)
-                player->set_state("left");
-            else if (move.x > K_EPSILON)
-                player->set_state("right");
-
-            move *= 100.0f * t.s();
-            player->clear_snapshots();
-            player->snapshot();
-            player->move(move);
-        }
-        else {
-            if (not in_air)
-                player->set_state("stand");
-
-            player->clear_snapshots();
-            player->snapshot();
-        }
-    }
-
     m_pRoot->logic(t);
     m_pOrthoRoot->logic(t);
 }
-
-
-void Game :: shoot(Sprite* origin) {
-    auto shot = make_shared<Mesh>(
-        make_shared<MeshGeometry>(Prefab::quad(glm::vec2(8.0f, 2.0f))),
-        vector<shared_ptr<IMeshModifier>>{
-            make_shared<Wrap>(Prefab::quad_wrap(
-                glm::vec2(0.0f, 1.0f), glm::vec2(1.0f, 0.0f)
-            ))
-        },
-        make_shared<MeshMaterial>("laser.png", m_pResources)
-    );
-
-    shot->material()->emissive(Color::white());
-    m_pRoot->add(shot);
-
-    shot->position(glm::vec3(
-        origin->position().x +
-        -origin->origin().x*origin->size().x +
-            origin->mesh()->world_box().size().x / 2.0f,
-        origin->position().y +
-            -origin->origin().y*origin->size().y +
-            origin->mesh()->world_box().size().y / 2.0f + -2.0f,
-        origin->position().z
-    ));
-
-    shot->rotate(((std::rand() % 10)-5) / 360.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-    shot->velocity(shot->orient_to_world(glm::vec3(
-        (origin->check_state("left") ? -1.0f : 1.0f) * 256.0f, 0.0f, 0.0f
-    )));
-
-    auto timer = make_shared<Freq::Alarm>(m_pQor->timer()->timeline());
-    timer->set(Freq::Time::seconds(0.5f));
-
-    auto shotptr = shot.get();
-    shot->on_tick.connect([timer, shotptr](Freq::Time t){
-        if (timer->elapsed())
-            shotptr->detach();
-    });
-    
-    m_pPartitioner->register_object(shot, BULLET);
-    
-    Sound::play(m_pCamera.get(), "shoot.wav", m_pResources);
-
-    m_ShootTimer.set(Freq::Time::ms(m_Power == 0 ? 200 : 100));
-    
-    // increase box Z width
-    auto shotbox = shot->box();
-
-    shot->set_box(Box(
-        vec3(shotbox.min().x, shotbox.min().y, -5.0),
-        vec3(shotbox.max().x, shotbox.max().y, 5.0)
-    ));
-}
-
 
 void Game :: render() const {
     m_pPipeline->override_shader(PassType::NORMAL, m_Shader);
--- src/Player.h
+++ src/Player.h
@@ -2,22 +2,58 @@
 #define PLAYER_H_E74SGBRG
 
 #include "Qor/Sprite.h"
+#include "Qor/Input.h"
+#include "Qor/Camera.h"
 
-class Player:
-    public Sprite
-{
+class Game;
+
+class Player: public Sprite {
+    
     public:
 
-        //Player();
-        //virtual ~Player();
+        Player(
+            std::string fn,
+            Cache<Resource, std::string>* resources,
+            Camera* camera,
+            Controller* ctrl,
+            Freq::Timeline* timeline,
+            IPartitioner* part,
+            Game* game
+        );
+        virtual ~Player();
 
-        //Player(const Player&) = default;
-        //Player(Player&&) = default;
-        //Player& operator=(const Player&) = default;
-        //Player& operator=(Player&&) = default;
+        void enter();
+        
+        virtual void logic_self(Freq::Time t) override;
+        //virtual void render(Pass* pass) const override;
 
+        std::shared_ptr<Node> focus_right() { return m_pCharFocusRight; };
+        std::shared_ptr<Node> focus_left() { return m_pCharFocusLeft; };
+        
+        void shoot();
+        void battery(int b) { m_Power += b; }
+        
     private:
+        
+        Freq::Timeline* m_pTimeline;
+        
+        Cache<Resource, std::string>* m_pResources;
+        
+        std::shared_ptr<Node> m_pCharFocusLeft;
+        std::shared_ptr<Node> m_pCharFocusRight;
+        
+        Camera* m_pCamera;
+        int m_LastWallJumpDir = 0;
+        Freq::Alarm m_JumpTimer;
+        Freq::Alarm m_ShootTimer;
+        bool m_WasInAir = false;
+        unsigned m_Power = 0;
+        
+        Controller* m_pController;
+        IPartitioner* m_pPartitioner;
 
+        Game* m_pGame;
 };
 
 #endif
+
