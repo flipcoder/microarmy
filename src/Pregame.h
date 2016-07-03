df89cca055674888ce9a30f0893e7e7475a54df6
Kevin Nelson <knelsonvt@gmail.com>
2016-07-02 21:27:12 -0400

    Header cleanup and ray casting

--- src/Pregame.h
+++ src/Pregame.h
@@ -11,40 +11,44 @@
 #include "Qor/Sound.h"
 #include "Qor/Canvas.h"
 
+
 class Qor;
 
 class Pregame: public State {
     public:
-        
+        // Constructor
         Pregame(Qor* engine);
+
+
+        // Destructor
         virtual ~Pregame();
 
+
+        // Overriden virtual methods
         virtual void preload() override;
         virtual void enter() override;
         virtual void logic(Freq::Time t) override;
         virtual void render() const override;
-        virtual bool needs_load() const override {
-            return true;
-        }
+        virtual bool needs_load() const override { return true; }
+
 
     private:
-        
+        // Variables
+        bool m_Win = false;
+        Freq::Alarm m_Transition;
+
+
+        // Pointers
         Qor* m_pQor = nullptr;
         Input* m_pInput = nullptr;
         Pipeline* m_pPipeline = nullptr;
         ResourceCache* m_pResources = nullptr;
+        Controller* m_pController = nullptr;
 
         std::shared_ptr<Node> m_pRoot;
         std::shared_ptr<Camera> m_pCamera;
         std::shared_ptr<Sound> m_pMusic;
-        
-        Controller* m_pController = nullptr;
-
-        Freq::Alarm m_Transition;
-
         std::shared_ptr<Canvas> m_pCanvas;
-
-        bool m_Win = false;
 };
 
 #endif
