5215197cb94e150671aabd10dda8d629181c11e8
Grady O'Connell <flipcoder@gmail.com>
2016-08-06 02:06:36 -0700

    Updated premake4 to build better on windows, new qor

--- premake4.lua
+++ premake4.lua
@@ -5,14 +5,16 @@ solution("microarmy")
 
         -- Debug Config
         configuration "Debug"
-            defines { "DEBUG", "BACKWARD_HAS_BFD=1", "GLM_FORCE_RADIANS", "QOR_NO_PYTHON" }
+            defines { "DEBUG", "BACKWARD_HAS_BFD=1", "GLM_FORCE_RADIANS", "QOR_NO_PYTHON", "QOR_NO_PHYSICS" }
             flags { "Symbols" }
-            links {
-                "z",
-                "bfd",
-                "iberty"
-            }
             linkoptions { }
+
+            configuration "gmake"
+                links {
+                    "z",
+                    "bfd",
+                    "iberty"
+                }
 
         -- Release Config
         configuration "Release"
@@ -25,43 +27,163 @@ solution("microarmy")
             buildoptions { "-std=c++11" }
             -- Uncomment the following line to get in depth debugging 
             -- buildoptions { "-std=c++11", "-pedantic", "-Wall", "-Wextra", '-v', '-fsyntax-only'}
+            links {
+                "pthread",
+                "GL",
+                "GLU",
+                "SDL2",
+                "GLEW",
+                "assimp",
+                "freeimage",
+                "openal",
+                "alut",
+                "ogg",
+                "vorbis",
+                "vorbisfile",
+                "boost_system",
+                "boost_thread",
+                "boost_filesystem",
+                "boost_python",
+                "boost_coroutine",
+                "boost_regex",
+                "jsoncpp",
+                "BulletSoftBody",
+                "BulletDynamics",
+                "BulletCollision",
+                "LinearMath",
+                "z",
+                "RakNetDLL",
+            }
+            includedirs {
+                "lib/Qor/",
+                "lib/Qor/lib/kit",
+                "/usr/local/include/",
+                "/usr/include/bullet/",
+                "/usr/include/raknet/DependentExtensions"
+            }
 
+            libdirs {
+                "/usr/local/lib",
+                "/usr/local/lib64/",
+            }
+            
+            buildoptions {
+                "`python2-config --includes`",
+                "`pkg-config --cflags cairomm-1.0 pangomm-1.4`"
+            }
+
+            linkoptions {
+                "`python2-config --libs`",
+                "`pkg-config --libs cairomm-1.0 pangomm-1.4`"
+            }
         -- OS X Config
         configuration "macosx"
             buildoptions { "-U__STRICT_ANSI__", "-stdlib=libc++" }
             linkoptions { "-stdlib=libc++" }
 
+		configuration "windows"
+            links {
+                "ws2_32",
+                "glibmm-vc100-2_4",
+                "cairomm-vc100-1_0",
+                "pangomm-vc100-1_4",
+                "SDL2main",
+                "OpenGL32",
+                "GLU32",
+                "SDL2",
+                "GLEW32",
+                "assimp",
+                "freeimage",
+                "openal32",
+                "alut",
+                "libogg",
+                "libvorbis",
+                "libvorbisfile",
+                "boost_system-vc140-mt-gd-1_61",
+                "boost_thread-vc140-mt-gd-1_61",
+                "boost_filesystem-vc140-mt-gd-1_61",
+                "boost_python-vc140-mt-gd-1_61",
+                "boost_coroutine-vc140-mt-gd-1_61",
+                "boost_regex-vc140-mt-gd-1_61",
+                "lib_json",
+                "BulletSoftBody_vs2010",
+                "BulletDynamics_vs2010",
+                "BulletCollision_vs2010",
+                "LinearMath_vs2010",
+                "RakNet_VS2008_LibStatic_Debug_Win32",
+            }
+            
+		    includedirs {
+				"c:/Python27/include",
+				"c:/gtkmm/include/atk-1.0",
+				"c:/gtkmm/include/atkmm-1.6",
+				"c:/gtkmm/include/cairo",
+				"c:/gtkmm/include/cairomm-1.0",
+				"c:/gtkmm/include/fontconfig",
+				"c:/gtkmm/include/freetype2",
+				"c:/gtkmm/include/gail-1.0",
+				"c:/gtkmm/include/gdkmm-2.4",
+				"c:/gtkmm/include/gdk-pixbuf-2.0",
+				"c:/gtkmm/include/giomm-2.4",
+				"c:/gtkmm/include/glib-2.0",
+				"c:/gtkmm/include/glibmm-2.4",
+				"c:/gtkmm/include/gtk-2.0",
+				"c:/gtkmm/include/gtkmm-2.4",
+				"c:/gtkmm/include/libglade-2.0",
+				"c:/gtkmm/include/libglademm-2.4",
+				"c:/gtkmm/include/libpng14",
+				"c:/gtkmm/include/libxml2",
+				"c:/gtkmm/include/libxml++-2.6",
+				"c:/gtkmm/include/pango-1.0",
+				"c:/gtkmm/include/pangomm-1.4",
+				"c:/gtkmm/include/sigc++-2.0",
+				"c:/gtkmm/lib/atk-1.0/include",
+				"c:/gtkmm/lib/atkmm-1.6/include",
+				"c:/gtkmm/lib/cairo/include",
+				"c:/gtkmm/lib/cairomm-1.0/include",
+				"c:/gtkmm/lib/fontconfig/include",
+				"c:/gtkmm/lib/freetype2/include",
+				"c:/gtkmm/lib/gail-1.0/include",
+				"c:/gtkmm/lib/gdkmm-2.4/include",
+				"c:/gtkmm/lib/gdk-pixbuf-2.0/include",
+				"c:/gtkmm/lib/giomm-2.4/include",
+				"c:/gtkmm/lib/glib-2.0/include",
+				"c:/gtkmm/lib/glibmm-2.4/include",
+				"c:/gtkmm/lib/gtk-2.0/include",
+				"c:/gtkmm/lib/gtkmm-2.4/include",
+				"c:/gtkmm/lib/libglade-2.0/include",
+				"c:/gtkmm/lib/libglademm-2.4/include",
+				"c:/gtkmm/lib/libpng14/include",
+				"c:/gtkmm/lib/libxml2/include",
+				"c:/gtkmm/lib/libxml++-2.6/include",
+				"c:/gtkmm/lib/pango-1.0/include",
+				"c:/gtkmm/lib/pangomm-1.4/include",
+				"c:/gtkmm/lib/sigc++-2.0/include",
+				"c:/gtkmm/include/freetype2",
+				"c:/gtkmm/include/sigc++-2.0",
+				"c:/gtkmm/lib/sigc++-2.0/include",
+				"c:/Program Files (x86)/OpenAL 1.1 SDK/include",
+				"c:/msvc/include",
+				"c:/gtkmm/include/pangomm-1.4",
+				"c:/gtkmm/lib/pangomm-1.4/include",
+				"c:/gtkmm/include/pango-1.0",
+				"c:/gtkmm/include/cairo",
+				"c:/gtkmm/lib/cairomm-1.0/include",
+				"c:/gtkmm/include/cairomm-1.0",
+				"c:/gtkmm/include",
+				"c:/local/boost_1_61_0",
+			}
+			libdirs {
+				"c:/msvc/lib32/debug",
+				"c:/Program Files (x86)/OpenAL 1.1 SDK/libs/Win32",
+				"c:/msvc/lib32",
+				"c:/gtkmm/lib",
+				"c:/local/boost_1_61_0/lib32-msvc-14.0",
+			}
 
     project "microarmy"
         kind "WindowedApp"
         language "C++"
-        links {
-            "pthread",
-            "GL",
-            "GLU",
-            "SDL2",
-            "GLEW",
-            "assimp",
-            "freeimage",
-            "openal",
-            "alut",
-            "ogg",
-            "vorbis",
-            "vorbisfile",
-            "boost_system",
-            "boost_thread",
-            "boost_filesystem",
-            "boost_python",
-            "boost_coroutine",
-            "boost_regex",
-            "jsoncpp",
-            "BulletSoftBody",
-            "BulletDynamics",
-            "BulletCollision",
-            "LinearMath",
-            "z",
-            "RakNetDLL",
-        }
 
         -- Project Files
         files {
@@ -84,7 +206,7 @@ solution("microarmy")
             "lib/Qor/lib/kit/tests/**",
             "lib/Qor/lib/kit/toys/**"
         }
-
+        
         includedirs {
             "lib/Qor/",
             "lib/Qor/lib/kit",
@@ -93,51 +215,9 @@ solution("microarmy")
             "/usr/include/raknet/DependentExtensions"
         }
 
-        libdirs {
-            "/usr/local/lib",
-            "/usr/local/lib64/",
-        }
-        
-        buildoptions {
-            "`python2-config --includes`",
-            "`pkg-config --cflags cairomm-1.0 pangomm-1.4`"
-        }
-
-        linkoptions {
-            "`python2-config --libs`",
-            "`pkg-config --libs cairomm-1.0 pangomm-1.4`"
-        }
-
     project "test"
         kind "ConsoleApp"
         language "C++"
-        links {
-            "pthread",
-            "GL",
-            "GLU",
-            "SDL2",
-            "GLEW",
-            "assimp",
-            "freeimage",
-            "openal",
-            "alut",
-            "ogg",
-            "vorbis",
-            "vorbisfile",
-            "boost_system",
-            "boost_thread",
-            "boost_filesystem",
-            "boost_python",
-            "boost_coroutine",
-            "boost_regex",
-            "jsoncpp",
-            "BulletSoftBody",
-            "BulletDynamics",
-            "BulletCollision",
-            "LinearMath",
-            "z",
-            "RakNetDLL",
-        }
 
         -- Project Files
         files {
@@ -162,27 +242,12 @@ solution("microarmy")
             "lib/Qor/lib/kit/tests/**",
             "lib/Qor/lib/kit/toys/**"
         }
-
+        
         includedirs {
             "lib/Qor/",
             "lib/Qor/lib/kit",
             "/usr/local/include/",
             "/usr/include/bullet/",
             "/usr/include/raknet/DependentExtensions"
-        }
-
-        libdirs {
-            "/usr/local/lib",
-            "/usr/local/lib64/",
-        }
-        
-        buildoptions {
-            "`python2-config --includes`",
-            "`pkg-config --cflags cairomm-1.0 pangomm-1.4`"
-        }
-
-        linkoptions {
-            "`python2-config --libs`",
-            "`pkg-config --libs cairomm-1.0 pangomm-1.4`"
         }
 
