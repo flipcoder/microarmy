e3d0e3f7d807d7b277b2fc11c6dc6a81b6665abf
Grady O'Connell <flipcoder@gmail.com>
2017-07-03 13:15:00 -0700

    now builds from cli on windows

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-solution("microarmy")
+workspace("microarmy")
     targetdir("bin")
     
     configurations {"Debug", "Release"}
@@ -7,7 +7,7 @@ solution("microarmy")
         -- Debug Config
         configuration "Debug"
             defines { "DEBUG", "BACKWARD_HAS_BFD=1" }
-            flags { "Symbols" }
+            symbols "On"
             linkoptions { }
 
             configuration "gmake"
@@ -80,6 +80,9 @@ solution("microarmy")
             linkoptions { "-stdlib=libc++" }
 
         configuration "windows"
+            toolset "v141"
+            flags { "MultiProcessorCompile" }
+
             links {
                 "ws2_32",
                 --"glibmm.dll",
@@ -98,74 +101,17 @@ solution("microarmy")
                 "libogg",
                 "libvorbis",
                 "libvorbisfile",
-                "boost_system-vc140-mt-1_61",
-                "boost_thread-vc140-mt-1_61",
-                "boost_python-vc140-mt-1_61",
-                "boost_coroutine-vc140-mt-1_61",
-                "boost_regex-vc140-mt-1_61",
                 "lib_json",
             }
 
             includedirs {
-                "c:/gtkmm/include/atk-1.0",
-                "c:/gtkmm/include/atkmm-1.6",
-                "c:/gtkmm/include/cairo",
-                "c:/gtkmm/include/cairomm-1.0",
-                "c:/gtkmm/include/fontconfig",
-                "c:/gtkmm/include/freetype2",
-                "c:/gtkmm/include/gail-1.0",
-                "c:/gtkmm/include/gdkmm-2.4",
-                "c:/gtkmm/include/gdk-pixbuf-2.0",
-                "c:/gtkmm/include/giomm-2.4",
-                "c:/gtkmm/include/glib-2.0",
-                "c:/gtkmm/include/glibmm-2.4",
-                "c:/gtkmm/include/gtk-2.0",
-                "c:/gtkmm/include/gtkmm-2.4",
-                "c:/gtkmm/include/libglade-2.0",
-                "c:/gtkmm/include/libglademm-2.4",
-                "c:/gtkmm/include/libpng14",
-                "c:/gtkmm/include/libxml2",
-                "c:/gtkmm/include/libxml++-2.6",
-                "c:/gtkmm/include/pango-1.0",
-                "c:/gtkmm/include/pangomm-1.4",
-                "c:/gtkmm/include/sigc++-2.0",
-                "c:/gtkmm/lib/atk-1.0/include",
-                "c:/gtkmm/lib/atkmm-1.6/include",
-                "c:/gtkmm/lib/cairo/include",
-                "c:/gtkmm/lib/cairomm-1.0/include",
-                "c:/gtkmm/lib/fontconfig/include",
-                "c:/gtkmm/lib/freetype2/include",
-                "c:/gtkmm/lib/gail-1.0/include",
-                "c:/gtkmm/lib/gdkmm-2.4/include",
-                "c:/gtkmm/lib/gdk-pixbuf-2.0/include",
-                "c:/gtkmm/lib/giomm-2.4/include",
-                "c:/gtkmm/lib/glib-2.0/include",
-                "c:/gtkmm/lib/glibmm-2.4/include",
-                "c:/gtkmm/lib/gtk-2.0/include",
-                "c:/gtkmm/lib/gtkmm-2.4/include",
-                "c:/gtkmm/lib/libglade-2.0/include",
-                "c:/gtkmm/lib/libglademm-2.4/include",
-                "c:/gtkmm/lib/libpng14/include",
-                "c:/gtkmm/lib/libxml2/include",
-                "c:/gtkmm/lib/libxml++-2.6/include",
-                "c:/gtkmm/lib/pango-1.0/include",
-                "c:/gtkmm/lib/pangomm-1.4/include",
-                "c:/gtkmm/lib/sigc++-2.0/include",
-                "c:/gtkmm/include/freetype2",
-                "c:/gtkmm/include/sigc++-2.0",
-                "c:/gtkmm/lib/sigc++-2.0/include",
-                "c:/gtkmm/include/pangomm-1.4",
-                "c:/gtkmm/lib/pangomm-1.4/include",
-                "c:/gtkmm/include/pango-1.0",
-                "c:/gtkmm/include/cairo",
-                "c:/gtkmm/lib/cairomm-1.0/include",
-                "c:/gtkmm/include/cairomm-1.0",
                 "c:/gtkmm/lib/pangomm/include",
                 "c:/gtkmm/lib/sigc++/include",
                 "c:/gtkmm/lib/cairomm/include",
                 "c:/gtkmm/include/pango",
                 "c:/gtkmm/include/pangomm",
                 "c:/gtkmm/include/sigc++",
+                "c:/gtkmm/include",
                 "c:/gtkmm/include/cairo",
                 "c:/gtkmm/lib/glib/include",
                 "c:/gtkmm/include/glib",
@@ -173,20 +119,19 @@ solution("microarmy")
                 "c:/gtkmm/include/glibmm",
                 "c:/gtkmm/include/cairomm",
                 "c:/gtkmm/include",
-                "c:/local/boost_1_61_0",
+
+                "c:/local/boost_1_64_0",
                 "c:/Program Files (x86)/OpenAL 1.1 SDK/include",
                 "c:/msvc/include",
             }
             configuration { "windows", "Debug" }
                 libdirs { "c:/msvc/lib32/debug" }
                 links {
-                    "libboost_filesystem-vc140-mt-gd-1_61",
                     "RakNet_VS2008_LibStatic_Debug_Win32",
                 }
             configuration {}
             configuration { "windows", "Release" }
                 links {
-                    "libboost_filesystem-vc140-mt-1_61",
                     "RakNet_VS2008_LibStatic_Release_Win32",
                 }
             configuration { "windows" }
@@ -195,7 +140,7 @@ solution("microarmy")
                 "c:/Program Files (x86)/OpenAL 1.1 SDK/libs/Win32",
                 "c:/msvc/lib32",
                 "c:/gtkmm/lib",
-                "c:/local/boost_1_61_0/lib32-msvc-14.0",
+                "c:/local/boost_1_64_0/lib32-msvc-14.1",
             }
             -- buildoptions {
                 -- "/MP",
